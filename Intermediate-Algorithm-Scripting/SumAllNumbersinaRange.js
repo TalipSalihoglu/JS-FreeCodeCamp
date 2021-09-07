49cd44f742c6821602bb2f4cb7707c185db72907
TalipSalihoglu <59333488+TalipSalihoglu@users.noreply.github.com>
2021-09-07 18:07:57 +0300

    updated

--- Intermediate-Algorithm-Scripting/SumAllNumbersinaRange.js
+++ Intermediate-Algorithm-Scripting/SumAllNumbersinaRange.js
@@ -2,7 +2,6 @@
 //We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
 
 function sumAll(arr) {
-    let sum=0
     let min=0
     let max=0
     if(arr[0]>arr[1]){
